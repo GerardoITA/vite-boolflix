e8f170697abd88337f13f4c3f1773a9de137141e
GerardoITA <gerardo.giusti@tiscali.it>
2022-12-17 15:06:38 +0100

    Creato template e componente search ( e pulizia generale di file iniziali)

---
+++ src/components/Search.vue
@@ -0,0 +1,21 @@
+<script>
+import store from "../store";
+
+export default {
+    name: "Search",
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use './style/general.scss';
+@use './style/partials/variables.scss';
+</style>
---
+++ src/components/TEMPLATE.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use './style/general.scss';
+@use './style/partials/variables.scss';
+</style>
